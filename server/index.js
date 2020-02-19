4542d81e8196b66f83c6ed7d1ace05407e2abc3e
wjloo95 <loo.williamj@gmail.com>
2020-02-19 12:30:42 -0500

    Initialize more server files

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+
+const cors = require('cors');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
 const app = express();
+
+const routes = require('./routes');
+
 const port = process.env.PORT || 12345;
 
+app.use(cors());
+app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+app.use('/reviews', routes);
 
 app.listen(port, err => {
   if (err) {
