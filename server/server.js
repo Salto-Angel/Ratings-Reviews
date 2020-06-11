95afccca25d25073a1241fe5e0a3ad54c2713d27
Will Loo <loo.williamj@gmail.com>
2020-06-11 13:29:02 -0400

    connect redis cache

--- server/server.js
+++ server/server.js
@@ -1,30 +1,24 @@
 const app = require('./index');
-// const redis = require('redis');
+const redis = require('redis');
 
-// const redis_port = process.env.PORT || 6379;
+const redis_port = 6379;
 const port = process.env.PORT || 12345;
 
-// const redisClient = redis.createClient(redis_port);
-// /*
-//  To start Redis Server:
-//   - Open Ubuntu WSL
-//   - sudo service redis-server start
-//   */
+const redisClient = redis.createClient(redis_port);
 
-// module.exports = { lol: 'lol' };
-// redisClient.on('connect', function() {
-//   console.log('Redis is connected');
-// });
+redisClient.on('connect', function () {
+  console.log('Redis is connected');
+});
 
-// redisClient.on('ready', function() {
-//   console.log('Redis is ready');
-// });
+redisClient.on('ready', function () {
+  console.log('Redis is ready');
+});
 
-// redisClient.on('error', function(err) {
-//   console.log('Something went wrong ', err);
-// });
+redisClient.on('error', function (err) {
+  console.log('Something went wrong ', err);
+});
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   console.log('Environment: ', process.env.NODE_ENV);
   if (err) {
     console.log(`Error connecting to ${port}`, err);
